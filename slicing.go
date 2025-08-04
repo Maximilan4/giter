e2b253bee552dc2314f9b73e72bdaa76fe830e0e
Max Maximov <maxim.maximov@corp.mail.ru>
2025-08-05 00:43:37 +0400

    feat: TakeWhile & TakeWhile2 for taking n elements by rule

--- slicing.go
+++ slicing.go
@@ -4,6 +4,44 @@ import (
 	"iter"
 )
 
+// TakeWhile - take elements from iter.Seq, until given func returns true
+func TakeWhile[WF func(T) bool, T any](seq iter.Seq[T], f WF) iter.Seq[T] {
+	if f == nil {
+		return seq
+	}
+
+	return func(yield func(T) bool) {
+		for item := range seq {
+			if !f(item) {
+				return
+			}
+
+			if !yield(item) {
+				return
+			}
+		}
+	}
+}
+
+// TakeWhile2 - take elements from iter.Seq2, until given func returns true
+func TakeWhile2[WF func(K, V) bool, K, V any](seq iter.Seq2[K, V], f WF) iter.Seq2[K, V] {
+	if f == nil {
+		return seq
+	}
+
+	return func(yield func(K, V) bool) {
+		for k, v := range seq {
+			if !f(k, v) {
+				return
+			}
+
+			if !yield(k, v) {
+				return
+			}
+		}
+	}
+}
+
 // Take - takes only n first elements from iter.Seq
 func Take[T any](seq iter.Seq[T], n int) iter.Seq[T] {
 	if n < 1 {
--- slicing_test.go
+++ slicing_test.go
@@ -4,6 +4,7 @@ import (
 	"maps"
 	"reflect"
 	"slices"
+	"strconv"
 	"testing"
 )
 
@@ -37,13 +38,15 @@ func TestTakeLessThanOne(t *testing.T) {
 }
 
 func TestTake2(t *testing.T) {
-	m := map[string]int{
-		"1": 1,
-		"2": 2,
-		"3": 3,
-		"4": 4,
+	orderedIter := func(yield func(k string, v int) bool) {
+		for i := 1; i <= 4; i++ {
+			if !yield(strconv.Itoa(i), i) {
+				return
+			}
+		}
+		return
 	}
-	itr := Take2(maps.All(m), 2)
+	itr := Take2(orderedIter, 2)
 
 	got := maps.Collect(itr)
 	exp := map[string]int{
@@ -75,5 +78,70 @@ func TestTake2LessThanOne(t *testing.T) {
 				t.Errorf("bad take - exp %v, got %v", exp, got)
 			}
 		})
+	}
+}
+
+func TestTakeWhile(t *testing.T) {
+	sl := []int{2, 4, 6, 8, 9, 10, 11, 12}
+	itr := TakeWhile(slices.Values(sl), func(i int) bool {
+		return (i % 2) == 0
+	})
+
+	got := slices.Collect(itr)
+	exp := []int{2, 4, 6, 8}
+	if !reflect.DeepEqual(exp, got) {
+		t.Errorf("bad take - exp %v, got %v", exp, got)
+	}
+}
+
+func TestTakeWhileFuncNil(t *testing.T) {
+	sl := []int{2, 4, 6, 8, 9, 10, 11, 12}
+	itr := TakeWhile(slices.Values(sl), nil)
+
+	got := slices.Collect(itr)
+	exp := sl
+	if !reflect.DeepEqual(exp, got) {
+		t.Errorf("bad take - exp %v, got %v", exp, got)
+	}
+}
+
+func TestTakeWhile2(t *testing.T) {
+	orderedIter := func(yield func(k string, v int) bool) {
+		for i := 1; i <= 4; i++ {
+			if !yield(strconv.Itoa(i), i) {
+				return
+			}
+		}
+		return
+	}
+
+	itr := TakeWhile2(orderedIter, func(k string, v int) bool {
+		return k == "1" || ((v%2) == 0 && k != "4")
+	})
+
+	got := maps.Collect(itr)
+	exp := map[string]int{
+		"1": 1,
+		"2": 2,
+	}
+
+	if !reflect.DeepEqual(exp, got) {
+		t.Errorf("bad take - exp %v, got %v", exp, got)
+	}
+}
+
+func TestTakeWhile2FuncNil(t *testing.T) {
+	m := map[string]int{
+		"1": 1,
+		"2": 2,
+		"3": 3,
+		"4": 4,
+	}
+	itr := TakeWhile2(maps.All(m), nil)
+
+	got := maps.Collect(itr)
+	exp := m
+	if !reflect.DeepEqual(exp, got) {
+		t.Errorf("bad take - exp %v, got %v", exp, got)
 	}
 }
